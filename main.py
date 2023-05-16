4b23e51532cd1cc8478a8a89bbbd5e366f8aa35d
Kurashov M <mkurashov08@gmail.com>
2023-05-16 09:22:30 +0300

    условие

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 arr = [16,-2,-5,8,-9,11,23,56,-7,6,12,-1,8,31,78] # массив
 sum = 0 #переменная
 for i in range(len(arr)): #цикл
-    if arr[i] < 0 and i % 3 == 0:
+    if arr[i] < 0 and i % 3 == 0:#условие
         sum += arr[i]
 print(i)
