9b29d145b5e760b4ab2c978e9dc687235b60d4fc
Kurashov M <mkurashov08@gmail.com>
2023-05-16 09:21:50 +0300

    цикл

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 arr = [16,-2,-5,8,-9,11,23,56,-7,6,12,-1,8,31,78] # массив
-sum = 0 #переменная1
-for i in range(len(arr)):
+sum = 0 #переменная
+for i in range(len(arr)): #цикл
     if arr[i] < 0 and i % 3 == 0:
         sum += arr[i]
 print(i)
